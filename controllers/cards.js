afd2d108b155c238ac5cdc75bd993b06d0d269a9
wKategianes <wKategianes@gmail.com>
2023-02-14 11:15:10 -0800

    Updated show.ejs

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,50 +7,49 @@ module.exports = {
     show
 };
 
-// async function show(req, res) {
-    
-//     try {
-//         const card = await
-//         fetch(`${MTG_URL}/${req.params.id}`)
-//         .then(res => res.json())
-//         .then(card => card.card)
-//         res.render('cards/show', {title: 'Card Details', card})
-//         console.log(card, "This is the individual Card.");
-
-//     } catch (error) {
-//         const card = null;
-//         console.log(error, "This is the error of try/catch.");
-//         res.render('cards/show', {title: 'Card Details', card, error})        
-        
-//     }
-// }
-
 async function show(req, res) {
-    // let card = await Card.findOne({cardId: req.params.id});
-    // console.log(card); 
     
     try {
-  
-            let card = await
-            fetch(`${MTG_URL}/${req.params.id}`)
-            .then(res => res.json())
-            .then(card => card.card)
-            const newCard = {
-                name: card.name,
-                set: card.set,
-                imageURL: card.imageUrl,
-                cardId: card.id
-            }
-            console.log(card);
-            card = Card.create(newCard);
-            res.render('cards/show', {title: 'Card Details', card})
-            console.log(card);
-                    
+        const card = await
+        fetch(`${MTG_URL}/${req.params.id}`)
+        .then(res => res.json())
+        .then(card => card.card)
+        res.render('cards/show', {title: 'Card Details', card})
+        console.log(card, "This is the individual Card.");
+
     } catch (error) {
-        console.log(error);
-        res.render('cards/show', {title: 'Card Details', card, error})        
+        const card = null;
+        console.log(error, "This is the error of try/catch.");
+        res.render('cards/show', {title: 'Card Details', card, error})
     }
 }
+
+// async function show(req, res) {
+//     // let card = await Card.findOne({cardId: req.params.id});
+//     // console.log(card); 
+    
+//     try {
+  
+//             let card = await
+//             fetch(`${MTG_URL}/${req.params.id}`)
+//             .then(res => res.json())
+//             .then(card => card.card)
+//             let newCard = {
+//                 name: card.name,
+//                 set: card.set,
+//                 imageURL: card.imageUrl,
+//                 cardId: card.id
+//             }
+//             console.log(card);
+//             card = Card.create(newCard);
+//             res.render('cards/show', {title: 'Card Details', card})
+//             console.log(card);
+
+//     } catch (error) {
+//         console.log(error);
+//         res.render('cards/show', {title: 'Card Details', card, error})        
+//     }
+// }
 
 async function index(req, res) {
     const name = req.query.name;
